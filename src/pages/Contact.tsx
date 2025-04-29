bc9d36c0546be05cddfafc61ab870e08ba45d758
Josh Demers <jdemers0125@gmail.com>
2025-04-29 11:16:10 -0500

    Finished taking out Process

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,8 @@
 import { Contact as ContactComponent } from "../components/Contact";
-import { Process } from "../components/Process";
 
 const Contact = () => {
   return (
     <main className="pt-20">
-      <Process />
       <ContactComponent />
     </main>
   );
