6b009386cddde3ddfabaca80f9d44e4d17684fc6
Josh Demers <jdemers0125@gmail.com>
2025-05-30 17:11:45 -0500

    Same

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,8 @@
-import { Promo } from "../components/Promo";
 import { Consultation } from "../components/Consultation";
 
 const Contact = () => {
   return (
     <main className="pt-20">
-      <Promo />
       <Consultation />
     </main>
   );
