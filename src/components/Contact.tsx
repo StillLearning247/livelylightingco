7464de0fd7e8471dc7c964f5d604d31ce5cd3990
Josh Demers <jdemers0125@gmail.com>
2025-05-30 17:07:48 -0500

    Deleted Promo

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,8 @@
-// import { Promo } from "../components/Promo";
 import { Consultation } from "../components/Consultation";
 
 const Contact = () => {
   return (
     <>
-      {/* <Promo /> */}
       <Consultation />
     </>
   );
