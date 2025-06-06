6fad50c3ac723b0779b35c4347eba874c8f6fecc
Josh Demers <jdemers0125@gmail.com>
2025-06-06 09:30:30 -0500

    Added back Hero img

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ import { quality } from "@cloudinary/url-gen/actions/delivery";
 
 export const Hero = () => {
   // Configure the Cloudinary image
-  const heroImage = cld.image("House6_WebP_g9jtmj"); // Replace with your Cloudinary public ID
+  const heroImage = cld.image("House6_kmwq4e"); // Replace with your Cloudinary public ID
 
   heroImage
     .format("auto") // Use WebP/AVIF automatically
